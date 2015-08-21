55e72b3e110f82de5204055d9a3ef06c69bc36c5
naotake.k <naotake.87@gmail.com>
2015-08-21 15:14:00 +0900

    模範解答を追加

--- src/test/java/salarycalculation/domain/EmployeeDomainTest_Mockito.java
+++ src/test/java/salarycalculation/domain/EmployeeDomainTest_Mockito.java
@@ -1,5 +1,19 @@
 package salarycalculation.domain;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import salarycalculation.entity.Employee;
+
 /**
  * {@link EmployeeDomain}に対する Mockito を使ったテストクラス。
  *
@@ -7,4 +21,44 @@ package salarycalculation.domain;
  */
 public class EmployeeDomainTest_Mockito {
 
+    private EmployeeDomain testee;
+
+    /**
+     * 事前処理。
+     */
+    @Before
+    public void setUp() {
+        testee = mock(EmployeeDomain.class);
+    }
+
+    @Test
+    public void 入社年数を取得できること() {
+        // 振る舞いを定義
+        when(testee.getDurationMonth()).thenReturn(12, 24, 36);
+        when(testee.getDurationYear()).thenCallRealMethod();
+
+        // 実行
+        assertThat(testee.getDurationYear(), is(1)); // Month: 12
+        assertThat(testee.getDurationYear(), is(2)); // Month: 24
+        assertThat(testee.getDurationYear(), is(3)); // Month: 36
+
+        // 検証
+        verify(testee, times(3)).getDurationMonth();
+    }
+
+    @Test
+    public void spyを使用して入社年数を取得できること() {
+        testee = spy(new EmployeeDomain(new Employee()));
+
+        // 振る舞いを定義
+        doReturn(12).doReturn(24).doReturn(36).when(testee).getDurationMonth();
+
+        // 実行
+        assertThat(testee.getDurationYear(), is(1)); // Month: 12
+        assertThat(testee.getDurationYear(), is(2)); // Month: 24
+        assertThat(testee.getDurationYear(), is(3)); // Month: 36
+
+        // 検証
+        verify(testee, times(3)).getDurationMonth();
+    }
 }
---
+++ src/test/java/salarycalculation/sample/MockitoLearningTest.java
@@ -0,0 +1,101 @@
+package salarycalculation.sample;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.withSettings;
+
+import org.junit.Test;
+
+/**
+ * Mockito 学習用のテストクラス。
+ *
+ * @author naotake
+ */
+public class MockitoLearningTest {
+
+    private MockitoLearning testee;
+
+    @Test
+    public void 依存先のオブジェクトについても一括でモック化できること() throws Exception {
+        testee = mock(MockitoLearning.class, RETURNS_DEEP_STUBS);
+
+        // 依存先のオブジェクトまで振る舞いを定義
+        when(testee.getBar().getBaz().getName()).thenReturn("MOCK!!");
+
+        // 実行
+        assertThat(testee.getBar().getBaz().getName(), is("MOCK!!"));
+
+        // 検証
+        verify(testee.getBar().getBaz()).getName();
+    }
+
+    @Test
+    public <MultiMock extends InterfaceFoo & InterfaceBar> void 複数のインタフェースを継承したモックを作成できること()
+            throws Exception {
+
+        @SuppressWarnings("unchecked")
+        MultiMock testee = (MultiMock) mock(InterfaceFoo.class,
+                withSettings().extraInterfaces(InterfaceBar.class));
+
+        // 振る舞いを定義
+        when(testee.foo()).thenReturn("Foooo!!");
+        when(testee.bar()).thenReturn("Baaaar!!");
+
+        // 実行
+        assertThat(testee.foo(), is("Foooo!!"));
+        assertThat(testee.bar(), is("Baaaar!!"));
+
+        // 検証
+        verify(testee).foo();
+        verify(testee).bar();
+    }
+
+    private static class MockitoLearning {
+        private Bar bar;
+
+        @SuppressWarnings("unused")
+        public MockitoLearning() {
+            this.bar = new Bar();
+        }
+
+        public Bar getBar() {
+            return bar;
+        }
+    }
+
+    private static class Bar {
+        private Baz baz;
+
+        public Bar() {
+            this.baz = new Baz("MockitoLearning!!");
+        }
+
+        public Baz getBaz() {
+            return baz;
+        }
+    }
+
+    private static class Baz {
+        private String name;
+
+        public Baz(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    private static interface InterfaceFoo {
+        public String foo();
+    }
+
+    private static interface InterfaceBar {
+        public String bar();
+    }
+}
