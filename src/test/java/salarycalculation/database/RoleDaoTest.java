cb77a06401d63288e7c8e2f5193f064d691d5399
naotake <naotawool@users.noreply.github.com>
2015-11-22 20:58:41 +0900

    模範解答追加

--- src/test/java/salarycalculation/database/RoleDaoTest.java
+++ src/test/java/salarycalculation/database/RoleDaoTest.java
@@ -1,13 +1,20 @@
 package salarycalculation.database;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.hamcrest.core.Is.is;
 import static salarycalculation.matchers.RecordNotFoundExceptionMatcher.isClass;
 import static salarycalculation.matchers.RecordNotFoundExceptionMatcher.isKey;
 
+import java.util.Arrays;
+
+import org.assertj.core.api.Condition;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.experimental.runners.Enclosed;
 import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
 
 import salarycalculation.entity.Role;
 import salarycalculation.exception.RecordNotFoundException;
@@ -17,53 +24,151 @@ import salarycalculation.exception.RecordNotFoundException;
  *
  * @author naotake
  */
+@RunWith(Enclosed.class)
 public class RoleDaoTest {
 
-    private RoleDao testee;
+    public static class JUnit4を使った場合 {
 
-    @Rule
-    public ExpectedException expect = ExpectedException.none();
+        private RoleDao testee;
 
-    /**
-     * 事前処理。
-     */
-    @Before
-    public void setUp() {
-        testee = new RoleDao();
+        @Rule
+        public ExpectedException expect = ExpectedException.none();
+
+        /**
+         * 事前処理。
+         */
+        @Before
+        public void setUp() {
+            testee = new RoleDao();
+        }
+
+        @Test(expected = NullPointerException.class)
+        public void 引数が_null_の場合_NullPointerException_が発生すること() {
+            testee.get(null);
+        }
+
+        @Test(expected = NullPointerException.class)
+        public void 引数が空文字の場合_NullPointerException_が発生すること() {
+            testee.get("");
+        }
+
+        @Test
+        public void 引数が2桁より少ない場合_IllegalArgumentException_が発生すること() {
+            expect.expect(IllegalArgumentException.class);
+            expect.expectMessage(is("等級は 2 桁で指定してください[1]"));
+
+            testee.get("A");
+        }
+
+        @Test
+        public void 引数が2桁より大きい場合_IllegalArgumentException_が発生すること() {
+            expect.expect(IllegalArgumentException.class);
+            expect.expectMessage(is("等級は 2 桁で指定してください[3]"));
+
+            testee.get("123");
+        }
+
+        @Test
+        public void 存在しない等級を指定した場合に例外が送出されること() {
+            expect.expect(RecordNotFoundException.class);
+            expect.expect(isClass(Role.class));
+            expect.expect(isKey("XX"));
+
+            testee.get("XX");
+        }
     }
 
-    @Test(expected = NullPointerException.class)
-    public void 引数が_null_の場合_NullPointerException_が発生すること() {
-        testee.get(null);
-    }
+    public static class AssertJを使った場合 {
 
-    @Test(expected = NullPointerException.class)
-    public void 引数が空文字の場合_NullPointerException_が発生すること() {
-        testee.get("");
-    }
+        private RoleDao testee;
 
-    @Test
-    public void 引数が2桁より少ない場合_IllegalArgumentException_が発生すること() {
-        expect.expect(IllegalArgumentException.class);
-        expect.expectMessage(is("等級は 2 桁で指定してください[1]"));
+        /**
+         * 事前処理。
+         */
+        @Before
+        public void setUp() {
+            testee = new RoleDao();
+        }
 
-        testee.get("A");
-    }
+        @Test
+        public void 引数が_null_の場合_NullPointerException_が発生すること() {
+            // when
+            Throwable thrown = catchThrowable(() -> {
+                testee.get(null);
+            });
 
-    @Test
-    public void 引数が2桁より大きい場合_IllegalArgumentException_が発生すること() {
-        expect.expect(IllegalArgumentException.class);
-        expect.expectMessage(is("等級は 2 桁で指定してください[3]"));
+            // expect
+            assertThat(thrown).isInstanceOf(NullPointerException.class);
+        }
 
-        testee.get("123");
-    }
+        @Test
+        public void 引数が空文字の場合_NullPointerException_が発生すること() {
+            // when
+            Throwable thrown = catchThrowable(() -> {
+                testee.get("");
+            });
 
-    @Test
-    public void 存在しない等級を指定した場合に例外が送出されること() {
-        expect.expect(RecordNotFoundException.class);
-        expect.expect(isClass(Role.class));
-        expect.expect(isKey("XX"));
+            // expect
+            assertThat(thrown).isInstanceOf(NullPointerException.class);
+        }
 
-        testee.get("XX");
+        @Test
+        public void 引数が2桁より少ない場合_IllegalArgumentException_が発生すること() {
+            // when
+            Throwable thrown = catchThrowable(() -> {
+                testee.get("A");
+            });
+
+            // expect
+            assertThat(thrown).isInstanceOf(IllegalArgumentException.class)
+                              .hasMessage("等級は 2 桁で指定してください[1]");
+        }
+
+        @Test
+        public void 引数が2桁より大きい場合_IllegalArgumentException_が発生すること() {
+            // when
+            Throwable thrown = catchThrowable(() -> {
+                testee.get("123");
+            });
+
+            // expect
+            assertThat(thrown).isInstanceOf(IllegalArgumentException.class)
+                              .hasMessage("等級は 2 桁で指定してください[3]");
+        }
+
+        @Test
+        public void 存在しない等級を指定した場合に例外が送出されること() {
+            // when
+            Throwable thrown = catchThrowable(() -> {
+                testee.get("XX");
+            });
+
+            // expect
+            final TargetKeys keysXX = new TargetKeys("XX");
+            assertThat(thrown).isInstanceOf(RecordNotFoundException.class).is(targetRole).is(keysXX);
+        }
+
+        private final Condition<Throwable> targetRole = new Condition<Throwable>("target class") {
+
+            public boolean matches(Throwable actual) {
+                RecordNotFoundException casted = RecordNotFoundException.class.cast(actual);
+                return (casted.getTargetClass() == Role.class);
+            }
+        };
+
+        private static final class TargetKeys extends Condition<Throwable> {
+
+            private final Object[] expects;
+
+            public TargetKeys(Object... expects) {
+                as(Arrays.toString(expects));
+                this.expects = expects;
+            }
+
+            public boolean matches(Throwable actual) {
+                RecordNotFoundException casted = RecordNotFoundException.class.cast(actual);
+                return Arrays.equals(casted.getKey(), expects);
+            }
+        }
     }
 }
--- src/test/java/salarycalculation/domain/EmployeeRepositoryTest_AssertJ.java
+++ src/test/java/salarycalculation/domain/EmployeeRepositoryTest_AssertJ.java
@@ -1,14 +1,20 @@
 package salarycalculation.domain;
 
-import static org.assertj.core.api.Assertions.fail;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import static salarycalculation.assertions.MyProjectAssertions.assertThat;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.experimental.theories.DataPoint;
+
+import salarycalculation.assertions.MyProjectSoftAssertions;
+import salarycalculation.entity.Employee;
 
 /**
  * {@link EmployeeRepository}に対する AssertJ を使ったテストクラス。
@@ -38,20 +44,38 @@ public class EmployeeRepositoryTest_AssertJ {
         // 実行
         List<EmployeeDomain> actuals = testee.findAll();
 
-        // TODO 期待する社員番号の順番は「1, 2, 3, 4」
-        fail("未検証");
+        // 社員番号を取り出す
+        List<Integer> actualNos = actuals.stream().map(s -> s.getNo()).collect(Collectors.toList());
+
+        // JUnit4 での検証
+        assertThat(actualNos.size(), is(4));
+        assertThat(actualNos, contains(1, 2, 3, 4));
+
+        // AssertJ style
+        assertThat(actuals).extracting(EmployeeDomain::getNo)
+                           .hasSize(4)
+                           .containsSequence(1, 2, 3, 4);
     }
 
     /**
      * Hands-on: 2-2
      */
     @Test
-    public void 社員一覧を想定年収の昇順に取得できること() {
+    public void 社員一覧を想定年収の昇順に社員名を取得できること() {
         // 実行
         List<EmployeeDomain> actuals = testee.findAllOrderByAnnualSalary(true);
 
-        // TODO 期待する社員名の順番は「"東京 次郎", "愛媛 蜜柑", "大阪 太郎", "埼玉 花子"」
-        fail("未検証");
+        // 社員番号を取り出す
+        List<String> actualNames = actuals.stream().map(s -> s.getEntity().getName()).collect(Collectors.toList());
+
+        // JUnit4 での検証
+        assertThat(actualNames.size(), is(4));
+        assertThat(actualNames, contains("東京 次郎", "愛媛 蜜柑", "大阪 太郎", "埼玉 花子"));
+
+        // AssertJ style
+        assertThat(actuals).extracting(EmployeeDomain::getEntity).extracting(Employee::getName)
+                           .hasSize(4)
+                           .containsSequence("東京 次郎", "愛媛 蜜柑", "大阪 太郎", "埼玉 花子");
     }
 
     /**
@@ -68,11 +92,30 @@ public class EmployeeRepositoryTest_AssertJ {
         assertThat(actual.getCapability().getRank(), is(fixture.expectedCapabilityRank));
         assertThat(actual.getOrganization().getName(), is(fixture.expectedOrganizationName));
 
-        // TODO assertThat(actual).hasNo(fixture.expectedNo);
-        // TODO assertThat(actual).hasName(fixture.expectedName);
-        // TODO assertThat(actual).hasRoleRank(fixture.expectedRoleRank);
-        // TODO assertThat(actual).hasCapabilityRank(fixture.expectedCapabilityRank);
-        // TODO assertThat(actual).hasOrganizationRank(fixture.expectedOrganizationName);
+        // AssertJ style
+        assertThat(actual).hasNo(fixture.expectedNo).hasName(fixture.expectedName)
+                          .hasRoleRank(fixture.expectedRoleRank)
+                          .hasCapabilityRank(fixture.expectedCapabilityRank)
+                          .hasOrganizationRank(fixture.expectedOrganizationName);
+    }
+
+    /**
+     * Hands-on: 4-2
+     */
+    @Test
+    public void SoftAssertionsを使って社員番号を基に情報を取得できること() {
+        // 社員番号[1]の場合
+        EmployeeDomain actual = testee.get(fixture.no);
+
+        // AssertJ style
+        MyProjectSoftAssertions softly = new MyProjectSoftAssertions();
+
+        softly.assertThat(actual).hasNo(fixture.expectedNo).hasName(fixture.expectedName)
+              .hasRoleRank(fixture.expectedRoleRank)
+              .hasCapabilityRank(fixture.expectedCapabilityRank)
+              .hasOrganizationRank(fixture.expectedOrganizationName);
+
+        softly.assertAll();
     }
 
     static class Fixture {
@@ -91,7 +134,7 @@ public class EmployeeRepositoryTest_AssertJ {
         String expectedOrganizationName;
 
         public Fixture(String no, int expectedNo, String expectedName, String expectedRoleRank,
-                String expectedCapabilityRank, String expectedOrganizationName) {
+                       String expectedCapabilityRank, String expectedOrganizationName) {
             this.no = no;
             this.expectedNo = expectedNo;
             this.expectedName = expectedName;
