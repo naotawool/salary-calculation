ca7db3d380e85bb6d6bdf42cbb57c7f1a72f714f
naotake <naotawool@users.noreply.github.com>
2017-08-13 16:44:01 +0900

    依存コンポーネントのバージョンを最新化

--- src/main/java/salarycalculation/domain/organization/Organization.java
+++ src/main/java/salarycalculation/domain/organization/Organization.java
@@ -1,32 +1,32 @@
 package salarycalculation.domain.organization;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import salarycalculation.utils.BaseEntity;
 
 public class Organization extends BaseEntity<String> {
 
-    /** 組織コード */
-    private String code;
+	/** 組織コード */
+	private String code;
 
-    /** 組織名 */
-    private String name;
+	/** 組織名 */
+	private String name;
 
-    public Organization(String code, String name) {
-        this.code = code;
-        this.name = name;
-    }
+	public Organization(String code, String name) {
+		this.code = code;
+		this.name = name;
+	}
 
-    @Override
-    public String getId() {
-        return code;
-    }
+	@Override
+	public String getId() {
+		return code;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public boolean equalsName(String name) {
-        return StringUtils.equals(this.name, name);
-    }
+	public boolean equalsName(String name) {
+		return StringUtils.equals(this.name, name);
+	}
 }
--- src/main/java/salarycalculation/utils/BaseEntity.java
+++ src/main/java/salarycalculation/utils/BaseEntity.java
@@ -1,42 +1,42 @@
 package salarycalculation.utils;
 
-import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
 public abstract class BaseEntity<T> implements Entity<T> {
 
-    @Override
-    public boolean equals(Object obj) {
-        if (obj == null) {
-            return false;
-        }
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == null) {
+			return false;
+		}
 
-        if (!this.getClass().isInstance(obj)) {
-            return false;
-        }
+		if (!this.getClass().isInstance(obj)) {
+			return false;
+		}
 
-        @SuppressWarnings("unchecked")
-        Entity<T> other = (Entity<T>) obj;
+		@SuppressWarnings("unchecked")
+		Entity<T> other = (Entity<T>) obj;
 
-        if (this.getId() == null && other.getId() == null) {
-            return true;
-        }
+		if (this.getId() == null && other.getId() == null) {
+			return true;
+		}
 
-        if (this.getId() == null) {
-            return false;
-        }
+		if (this.getId() == null) {
+			return false;
+		}
 
-        return this.getId().equals(other.getId());
-    }
+		return this.getId().equals(other.getId());
+	}
 
-    @Override
-    public int hashCode() {
-        return HashCodeBuilder.reflectionHashCode(this.getId());
-    }
+	@Override
+	public int hashCode() {
+		return HashCodeBuilder.reflectionHashCode(this.getId());
+	}
 
-    @Override
-    public String toString() {
-        return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
-    }
+	@Override
+	public String toString() {
+		return ToStringBuilder.reflectionToString(this, ToStringStyle.SHORT_PREFIX_STYLE);
+	}
 }
